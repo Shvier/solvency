ecbf5e90ae3821db7c725a394a17dc8170a68221
Shvier <Shvier1894@gmail.com>
2023-09-11 09:44:43 -0400

    added arkworks & implemented interpolating P and committing to P

---
+++ src/main.rs
@@ -0,0 +1,121 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use ark_ec::{VariableBaseMSM, CurveGroup};
+use ark_ff::PrimeField;
+use ark_poly_commit::kzg10::{KZG10, Powers, VerifierKey, Proof};
+use ark_bls12_381::Bls12_381;
+use ark_poly::{DenseUVPolynomial, EvaluationDomain, GeneralEvaluationDomain, Evaluations, Polynomial};
+use ark_poly::univariate::DensePolynomial;
+use ark_ec::pairing::Pairing;
+use ark_std::{test_rng, start_timer, end_timer};
+use ark_std::rand::Rng;
+use ark_bls12_381::Fr as F;
+
+type UniPoly_381 = DensePolynomial<<Bls12_381 as Pairing>::ScalarField>;
+
+fn main() {
+    const DEGREE: usize = 64;
+
+    let rng = &mut test_rng();
+    let params = KZG10::<Bls12_381, UniPoly_381>::setup(DEGREE, false, rng).expect("Setup failed");
+    let powers_of_g = params.powers_of_g[..=DEGREE].to_vec();
+    let powers_of_gamma_g = (0..=DEGREE)
+        .map(|i| params.powers_of_gamma_g[&i])
+        .collect();
+    let powers = Powers {
+        powers_of_g: ark_std::borrow::Cow::Owned(powers_of_g),
+        powers_of_gamma_g: ark_std::borrow::Cow::Owned(powers_of_gamma_g),
+    };
+
+    type D = GeneralEvaluationDomain::<F>;
+    let domain = D::new(53).unwrap();
+    let vectors = generate_liabilities();
+    println!("number of vectors: {}", vectors.len());
+    let evaluations = Evaluations::from_vec_and_domain(vectors, domain);
+
+    let poly = evaluations.interpolate();
+    
+    println!("coeffs size: {}", poly.coeffs.len());
+
+    let point = F::from(0);
+    let value = poly.evaluate(&point);
+
+    let (com, r) = KZG10::<Bls12_381, UniPoly_381>::commit(&powers, &poly, None, None).expect("Commitment failed");
+    let (witness, _): (UniPoly_381, Option<UniPoly_381>) = KZG10::<Bls12_381, UniPoly_381>::compute_witness_polynomial(&poly, point, &r).unwrap();
+
+    let vk = VerifierKey {
+        g: params.powers_of_g[0],
+        gamma_g: params.powers_of_gamma_g[&0],
+        h: params.h,
+        beta_h: params.beta_h,
+        prepared_h: params.prepared_h.clone(),
+        prepared_beta_h: params.prepared_beta_h.clone(),
+    };
+
+    let (num_leading_zeros, witness_coeffs) = skip_leading_zeros_and_convert_to_bigints(&witness);
+    let witness_comm_time = start_timer!(|| "Computing commitment to witness polynomial");
+    let w = <<Bls12_381 as Pairing>::G1 as VariableBaseMSM>::msm_bigint(
+        &powers.powers_of_g[num_leading_zeros..],
+        &witness_coeffs,
+    );
+    end_timer!(witness_comm_time);
+    let proof = Proof {
+        w: w.into_affine(),
+        random_v: None,
+    };
+
+    let result = KZG10::<Bls12_381, UniPoly_381>::check(&vk, &com, point, value, &proof).unwrap();
+    println!("{}", result);
+}
+
+fn skip_leading_zeros_and_convert_to_bigints<F: PrimeField, P: DenseUVPolynomial<F>>(
+    p: &P,
+) -> (usize, Vec<F::BigInt>) {
+    let mut num_leading_zeros = 0;
+    while num_leading_zeros < p.coeffs().len() && p.coeffs()[num_leading_zeros].is_zero() {
+        num_leading_zeros += 1;
+    }
+    let coeffs = convert_to_bigints(&p.coeffs()[num_leading_zeros..]);
+    (num_leading_zeros, coeffs)
+}
+
+fn convert_to_bigints<F: PrimeField>(p: &[F]) -> Vec<F::BigInt> {
+    let to_bigint_time = start_timer!(|| "Converting polynomial coeffs to bigints");
+    let coeffs = ark_std::cfg_iter!(p)
+        .map(|s| s.into_bigint())
+        .collect::<Vec<_>>();
+    end_timer!(to_bigint_time);
+    coeffs
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+fn generate_liabilities() -> Vec<F> {
+    let rng = &mut test_rng();
+
+    let usernames: Vec<char> = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    let liabilities: Vec<u64> = usernames.iter()
+        .map(|_| { rng.gen_range(0..2_u64.pow(15)) })
+        .collect();
+    println!("{:?}", liabilities);
+
+    let total: u64 = liabilities.iter().copied().sum();
+    println!("total: {}", total);
+
+    let mut vectors: Vec<F> = Vec::new();
+
+    for (username, liability) in usernames.into_iter().zip(liabilities.into_iter()).rev() {
+        let id = calculate_hash(&username);
+        let bal = F::from(liability);
+        vectors.push(bal);
+        vectors.push(F::from(id));
+    }
+
+    vectors.push(F::from(total));
+    vectors.reverse();
+    vectors
+}
